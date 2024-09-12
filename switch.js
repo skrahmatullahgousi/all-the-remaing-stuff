4535d028fdbba2778aa94116120d5a8d8160d08a
skrahmath <skrahmathgousi@gmail.com>
2024-09-12 19:48:43 +0530

    working

---
+++ switch.js
@@ -0,0 +1,12 @@
+let f=8;
+switch (f) {
+    case (f > 10):
+        console.log("No");
+        break;
+    case (f == 8):
+        console.log("Yes");
+        break;
+    default:
+        console.log("OK");
+        break;
+}
